64b886914a0980f9ebf521d721c6f4e5919dafd8
Anton Ageev <antforwork@mail.ru>
2020-02-22 09:54:12 +0300

    added box from Generics

--- src/lesson1Generic/Box.java
+++ src/lesson1Generic/Box.java
@@ -30,19 +30,21 @@ public class Box <T extends Fruit> {
 
     public void moveFruitsTo (Box<T> anotherBox){
         // Поставил проверку целевой коробки, чтобы Collections.copy работало. А оно не работает...
+        if (this == anotherBox) return;
         if (anotherBox.getWeight()>0){
             System.out.println("необходимо освободить целевую коробку... Ну или убрать эту проверку на непустую коробку");
             return;
         }
         anotherBox.fruitBoxList.ensureCapacity(this.fruitBoxList.size());
-        //Collections.copy(anotherBox.fruitBoxList, this.fruitBoxList);
+        Collections.copy(anotherBox.fruitBoxList, this.fruitBoxList);
         // почему-то метод Collections.copy выбрасывает IndexOutOfBoundsException.
         // в описании метода написано, что такой Exception бросается, если не хватает размера dest.
         // а почему его не хватает, если я сделал ensureCapacity по коробке src ?
         // пришлось пересыпать фрукты циклом.
-        for (T o : this.fruitBoxList){
-            anotherBox.fruitBoxList.add(o);
-        }
+
+        //for (T o : this.fruitBoxList){
+        //    anotherBox.fruitBoxList.add(o);
+        //}
         this.fruitBoxList.clear();
     }
 
